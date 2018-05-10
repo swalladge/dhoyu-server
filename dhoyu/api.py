00f595d2709ac7d0d24a62ef18126d9541ee8673
Samuel Walladge <samuel@swalladge.id.au>
2018-05-10 16:22:29 +0930

    small fixes

--- dhoyu/api.py
+++ dhoyu/api.py
@@ -1,28 +1,29 @@
-import os
 import functools
 import json
-from datetime import datetime, timedelta
+import os
 import uuid
+from datetime import datetime, timedelta
 from pprint import pprint as pp
 
-from flask import (abort, g, url_for, request, redirect, Blueprint, jsonify,
-        send_from_directory)
-import jwt
+from flask import (Blueprint, abort, g, jsonify, redirect, request,
+                   send_from_directory, url_for)
 from werkzeug.utils import secure_filename
 
-from .models import User, Game, Image, Language
-from . import db, app
+import jwt
+
+from . import app, db
 from .decorators import token_required
+from .models import Game, Image, Language, User
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-@bp.route('/token', methods=('POST',))
+
+@bp.route('/token', methods=('POST', ))
 def get_token():
     data = request.json
 
     if data is None:
         abort(400)
-
 
     username = request.json.get('username', None)
     password = request.json.get('password', None)
@@ -43,13 +44,13 @@ def get_token():
 
     expires = datetime.utcnow() + timedelta(weeks=1)
 
-    token = jwt.encode({
-        'sub': username,
-        'exp': expires,
+    token = jwt.encode(
+        {
+            'sub': username,
+            'exp': expires,
         },
         app.config['SECRET_KEY'],
-        algorithm='HS256',
-    )
+        algorithm='HS256', )
 
     return jsonify({
         'token': token.decode('utf-8'),
@@ -57,15 +58,13 @@ def get_token():
     })
 
 
-
-@bp.route('/register', methods=('POST',))
+@bp.route('/register', methods=('POST', ))
 def register():
 
     data = request.json
 
     if data is None:
         abort(400)
-
 
     username = request.json.get('username', None)
     password = request.json.get('password', None)
@@ -90,14 +89,13 @@ def register():
     return jsonify({'msg': 'success'})
 
 
-
-@bp.route('/user', methods=('GET',))
+@bp.route('/user', methods=('GET', ))
 @token_required
 def user_info():
     return jsonify(g.user.get_info_dict())
 
 
-@bp.route('/user/<username>', methods=('GET',))
+@bp.route('/user/<username>', methods=('GET', ))
 @token_required
 def other_user_info(username):
 
@@ -107,6 +105,7 @@ def other_user_info(username):
 
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 def allowed_image_file(f) -> bool:
     filename = f.filename
@@ -120,17 +119,13 @@ def allowed_image_file(f) -> bool:
     return ok
 
 
-@bp.route('/games', methods=('POST',))
+@bp.route('/games', methods=('POST', ))
 @token_required
 def create_game():
-
-    # TODO: switch to base64 encoded image data and all inside json?
 
     data = request.json
     if data is None:
         abort(400, 'invalid json data')
-
-    pp(data)
 
     # json data shape example
     # {
@@ -143,7 +138,6 @@ def create_game():
     #   "word": "epul",
     #   "public": true,
     #   "language": "rop", // language code
-
 
     # retrieve and validate data
 
@@ -167,7 +161,6 @@ def create_game():
     if not isinstance(language, str) or not language:
         abort(400, 'invalid or missing language code')
 
-
     # TODO: support more language codes - this isn't part of mvp though
     SUPPORTED_LANGS = ['rop']
     if language not in SUPPORTED_LANGS:
@@ -183,8 +176,9 @@ def create_game():
         image_data = image.get('data', None)
         if not isinstance(image_data, str) or not image_data:
             abort(400, 'invalid or rejected image')
+        # TODO: ensure valid base64 data, validate image, resize/compress (Pillow?)
 
-    # save stage
+        # save stage
     for image in images:
         game.images.append(Image(image['data']))
 
@@ -193,29 +187,35 @@ def create_game():
     db.session.add(game)
     db.session.commit()
 
-    return jsonify({
-        'msg': 'success'
-    })
+    return jsonify({'msg': 'success'})
 
 
-@bp.route('/games', methods=('GET',))
+# TODO: consider query param or /<language_code>/games style for language filtering support
+@bp.route('/games', methods=('GET', ))
 @token_required
 def list_games():
 
-    # TODO: return array of games with word and id/url
+    # union( public games, author's games)
+    games = Game.query.filter(db.or_(Game.public == True, Game.author == g.user))
 
     return jsonify({
-        'msg': 'success'
-        })
+        'games': [
+            {
+                'id': game.id,
+                'word': game.word,
+                'public': game.public,
+                'language': game.language.name,
+                # thumbnail in future?
+            }
+            for game in games
+        ],
+    })
 
 
-
-@bp.route('/games/<name>', methods=('GET',))
+@bp.route('/games/<id>', methods=('GET', ))
 @token_required
-def get_game(name):
+def get_game(id_):
 
     # TODO: get a game by name/id and return (with attached audio/images)
 
-    return jsonify({
-        'msg': 'success'
-        })
+    return jsonify({'msg': 'success'})
